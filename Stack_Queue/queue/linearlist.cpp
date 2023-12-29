4192da3657eca621d4c93f916ab7b01c7965eee0
Shuvkant <weird426344@gmail.com>
2023-12-29 21:44:30 +0545

    adding deque with shifting

--- Stack_Queue/queue/linearlist.cpp
+++ Stack_Queue/queue/linearlist.cpp
@@ -51,6 +51,25 @@ void Display(queue *q)
         std::cout << std::endl;
     }
 }
+void dequeueShifting(queue *q)
+{
+    if (q->front > q->rear)
+    {
+
+        std::cout << "Queue is empty" << std::endl;
+        return;
+    }
+
+    else
+    {
+        for (int i = q->front; i < q->rear; i++)
+        {
+            q->data[i] = q->data[i + 1];
+        }
+        (q->rear)--;
+    }
+}
+
 int main()
 {
     int choice;
@@ -62,7 +81,7 @@ int main()
         // getchar();
         // getchar();
         system("clear");
-        std::cout << "1.Enqueue\n2.Dequeue\n3.Display\n4.Exit " << std::endl;
+        std::cout << "1.Enqueue\n2.Dequeue\n3.Dequeue(with shifting)\n4.Display\n5.Exit " << std::endl;
         std::cout << "Enter your choice: ";
         std::cin >> choice;
         switch (choice)
@@ -76,9 +95,13 @@ int main()
             Dequeue(&q);
             break;
         case 3:
+            dequeueShifting(&q);
+            break;
+
+        case 4:
             Display(&q); //
             break;
-        case 4:
+        case 5:
             exit(0);
         default:
             std::cout << "Invalid Input";
